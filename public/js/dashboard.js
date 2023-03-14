cf74285072c8aa9a5cba666931d6aac9c93dd4e1
Garves47 <124073916+Garves47@users.noreply.github.com>
2023-03-14 04:46:36 +0000

    Todo List Finishedd

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,6 +6,7 @@ const $$ = (selector) => document.getElementById(selector);
 const postalRegEx =
   /^[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d$/i;
 
+//Reset Button
 const onReset = (evt) => {
   evt.preventDefault();
   $("#notifications").checked = true;
@@ -14,8 +15,8 @@ const onReset = (evt) => {
   $("#temperature").value = "";
 };
 
+//Submit Button
 const onSubmit = (evt) => {
-  //TODO::Reset any errors before submitting
 
   let notifications = $("#notifications").checked;
   $$("setting_notifications").textContent = notifications ? "On" : "Off";
@@ -27,19 +28,27 @@ const onSubmit = (evt) => {
     }
   }
 
-  //TODO:: Validate the postal code with the Regular Expression,
-  //TODO:: Display an error if not valid
+  //Postal Code Check
   let location = $("#location").value;
   if(postalRegEx.test(location)){
-    $$("setting_lighting_mode").textContent = location;
+    $$("setting_location").textContent = location;
     $$("location_error").textContent = "Postal Code is good :)";
 
   }else{
     $$("location_error").textContent = "Please enter a valid postal Code >:(";
+    $$("location").textContent = ("");
+  }
+  
+  //Temperature Check
+  let temperature = $("#temperature").value;
+  if(temperature>5 && temperature<30 && temperature%0.5==0){
+    $$("setting_temperature").textContent = temperature;
+    $$("temperature_error").textContent = "Temperature is good :)";
+  }else{
+    $$("temperature_error").textContent = "Should be between 5 and 30";
+    $$("temperature").textContent = ("");
 
   }
-    //TODO:: Validate the temperature by checking the range and if it's a number
-    //TODO:: Display an error if not valid
 
     evt.preventDefault();
 };
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,33 +1,96 @@
 "use strict";
 
 const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.getElementById(selector);
+
 
 const emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 const passwordRegEx = /^(?=.*?[A-Z])(?=.*?[a-z]).{8,}$/;
 
+const time = new Date();
+
+//Reset Button
 const onReset = (evt) => {
-  //TODO:: Reset the reset-able fields
+  $("#first_name").value = "";
+  $("#last_name").value = "";
+  $("#email").value = "";
+  $("#dob").value = "";
+  $("#password").value = "";
+
 };
 
 const onSubmit = (evt) => {
-  resetErrors();
-
-  //TODO:: Use this boolean to keep track of any errors because you need to prevent the settings
-  //       from updating if even one field is wrong
+  //Submission field errors
   let formErrors = false;
+  //Check First and Last name
+  let firstName = $("#first_name").value;
+  let lastName = $("#last_name").value;
+  if(firstName == "" || lastName == ""){
+    formErrors = true;
+    $$("name_error").textContent = "Please enter a name";
+  }else{
+    $$("name_error").textContent = "Cool name!";
 
-  //TODO:: Make sure name fields are not empty
-
-  //TODO:: Validate email with the Regular Expression
-
-  //TODO:: Validate password with the Regular Expression
-  //TODO:: Validate passwords to be matching
-
-  //TODO:: Make sure date of birth is in the past.
-
-  //TODO:: check you formErrors boolean and update the fields if still false
-
+  }
+  //Check email
+  let email = $("#email").value;
+  if(emailRegEx.test(email)){
+    $$("email_error").textContent = "We will send you so many emails";
+  }else{
+    formErrors = true;
+    $$("email_error").textContent = "Please enter a valid email";
+    $$("email").textContent = ("");
+  }
+  //Check Password for validity, then for matching 
+  let password1 = $("#password").value;
+  let password2 = $("#confirm_password").value;
+  if(!passwordRegEx.test(password1)){
+    formErrors = true;
+    $$("password_error").textContent = "Please enter a valid password";
+    $$("password").textContent = ("");
+    $$("confirm_password").textContent = ("");
+  }else if(password1 != password2){
+    formErrors = true;
+    $$("password_error").textContent = "Please make sure both passwords match";
+    $$("password").textContent = ("");
+    $$("confirm_password").textContent = ("");
+  }else{
+    $$("password_error").textContent = "Sharing it with everyone on your contact list now;)";
+  }
+    
+  //Check that the birthday is less than the current time
+  let birthday = new Date($("#dob").value);
+  let birthdaySent = $("#dob").value;
+  let birthDay2 = birthday.getDay();
+  let birthMonth = birthday.getMonth();
+  let birthYear = birthday.getFullYear();
+  let checkDay = time.getDay();
+  let checkMonth = time.getMonth();
+  let checkYear = time.getFullYear();
+  if(birthYear>checkYear){
+    formErrors = true;
+    $$("dob_error").textContent = "You havn't been born yet?";
+    $$("dob").value = "";
+  }else if(birthMonth>checkMonth){
+    formErrors = true;
+    $$("dob_error").textContent = "You havn't been born yet?";
+    $$("dob").value = "";
+  }else if(birthDay2>checkDay){
+    formErrors = true;
+    $$("dob_error").textContent = "You havn't been born yet?";
+    $$("dob").value = "";
+  }else{
+    $$("dob_error").textContent = "Wait when was it again?";
+  }
+  //Check for error flags and Send all info
+  if(formErrors == false){
+    $$("user_first_name").textContent = firstName;
+    $$("user_last_name").textContent = lastName;
+    $$("user_email").textContent = email;
+    $$("user_dob").textContent = birthdaySent;
+    $$("user_password_last_changed").textContent = new Date().toDateString();
+  }
   evt.preventDefault();
 };
 
